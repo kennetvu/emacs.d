3ea79b8fc3632461fce6335ae065461df38d28a1
Kennet Vuong <kennet@nktconsulting.net>
2014-09-16 23:52:58 +0200

    Added a mac file

--- init.el
+++ init.el
@@ -1,6 +1,11 @@
-;; -----
-;; Emacs settings
-;; By Kennet Vuong
+;; init.el ---- Init file
+
+;; Author: Kennet Vuong
+;; URL: https://github.com/knyyy/emacs.d/
+;; Version: 0.1
+;; Created: 2014-09-16
+;; Credit: My files are created based on: 
+;; - Magnar Sveen : https://github.com/magnars/.emacs.d/
 ;; ----
 
 ;; General settings to be load before os check
@@ -9,10 +14,13 @@
 ;; Check wheter emacs is running on mac/linux or windows
 ;;Psudo code: if mac-> Load mac settings, else if gnu linux -> load gnu
 ;;Everything after if else, is general load
-;; change command to meta, and ignore option to use weird Norwegian keyboard
-(setq mac-option-modifier 'super)
-(setq mac-command-modifier 'meta)
-(setq ns-function-modifier 'hyper)
 
+;; Os check
+(if (eq system-type 'darwin)
+    (progn
+      ;; change command to meta, and ignore option to use weird Norwegian keyboard
+      (print "Running on mac")
+      (require 'mac)
+      ))
 
 ;; Settings to be load after os check
---
+++ mac.el
@@ -0,0 +1,13 @@
+;; mac.el ---- Emacs settings when running on mac
+
+;; Author: Kennet Vuong
+;; URL: https://github.com/knyyy/emacs.d/
+;; --------
+
+
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq ns-function-modifier 'hyper)
+
+
+(provide 'mac)
